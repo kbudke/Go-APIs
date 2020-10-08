2c5b078ead8528913cd333be12058d3b4ba264cd
Kyle Budke <kyle_budke@homedepot.com>
2020-10-08 17:33:19 -0400

    update main

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
-	. "github.homedepot.com/mentor-pairing/Go-APIs/models"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	hostname     = "localhost"
+	host_port    = 1234
+	username     = "starwars_user"
+	password     = "1234"
+	databasename = "StarWars"
 )
 
 func main() {
+	pg_con_string := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		hostname, host_port, username, password, databasename)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-	var person Person
+	// We can also ping our connection which will let us know if our connection is correct /// or not then we put an error-handling code right after that.
+	error = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 
+	fmt.Println("You are Successfully connected!")
 }
